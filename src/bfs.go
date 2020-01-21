df5d03de23435828b046d0fb40e627956ec1e711
Valentin Omnès <valentin.omnes@gmail.com>
2020-01-21 23:36:51 +0100

    feat: Clean the data path list to keep only the last step

--- src/bfs.go
+++ src/bfs.go
@@ -8,32 +8,36 @@ package main
 
 // BFS Travers a graph using BFS algorithm
 func (g *ItemGraph) BFS() {
+	// hasEdges is a boolean that allows to check if we need to remove the
+	// previous paths that have been updated
+	var hasEdges bool
 	g.lock.RLock()
+	visited := make(map[Node]bool)
+	// Init queue
 	q := NodeQueue{}
 	queue := q.New()
 	queue.Enqueue(*g.nodes[0])
-	visited := make(map[Node]bool)
+	// Init Path List
 	dp := DataPath{}
 	dataPath := dp.NewListPath()
 	dataPath.InsertNewPath(g.nodes[0].String())
-	hasEdges := false
 	for {
 		if queue.IsEmpty() {
 			break
 		}
 		currentNode := queue.Dequeue()
 		visited[*currentNode] = true
+		hasEdges = false
 		for _, childNode := range g.edges[*currentNode] {
 			if !visited[*childNode] {
 				queue.Enqueue(*childNode)
 				visited[*childNode] = true
 				dataPath.Growth(currentNode.String(), childNode.String())
-				// fmt.Println(currentNode.String(), childNode.String())
 				hasEdges = true
 			}
 		}
 		if hasEdges {
-			// dataPath.RemovePath(currentNode.String())
+			dataPath.RemovePath(currentNode.String())
 		}
 	}
 	dataPath.Print()
