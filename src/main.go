d584ebc862f0a42ee8bc5573d08d07a95b636547
Valentin Omnès <valentin.omnes@gmail.com>
2020-01-22 22:36:36 +0100

    feat: Add new graph in main

--- src/main.go
+++ src/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 var graph ItemGraph
 
 func fillGraph() *ItemGraph {
@@ -12,6 +10,11 @@ func fillGraph() *ItemGraph {
 	nE := Node{"E"}
 	nF := Node{"F"}
 	nG := Node{"G"}
+	nH := Node{"H"}
+	nI := Node{"I"}
+	nJ := Node{"J"}
+	nK := Node{"K"}
+	nL := Node{"L"}
 	graph.AddNode(&nA)
 	graph.AddNode(&nB)
 	graph.AddNode(&nC)
@@ -19,7 +22,11 @@ func fillGraph() *ItemGraph {
 	graph.AddNode(&nE)
 	graph.AddNode(&nF)
 	graph.AddNode(&nG)
-
+	graph.AddNode(&nH)
+	graph.AddNode(&nI)
+	graph.AddNode(&nJ)
+	graph.AddNode(&nK)
+	graph.AddNode(&nL)
 	// graph.AddEdge(&nA, &nB)
 	// graph.AddEdge(&nA, &nC)
 	// graph.AddEdge(&nB, &nE)
@@ -36,19 +43,45 @@ func fillGraph() *ItemGraph {
 	// graph.AddEdge(&nD, &nE)
 	// graph.AddEdge(&nD, &nG)
 	// graph.AddEdge(&nE, &nF)
+	// -->
+	// graph.AddEdge(&nA, &nB)
+	// graph.AddEdge(&nA, &nC)
+	// graph.AddEdge(&nA, &nE)
+	// graph.AddEdge(&nB, &nD)
+	// graph.AddEdge(&nB, &nF)
+	// graph.AddEdge(&nC, &nG)
+	// graph.AddEdge(&nE, &nF)
+	//					A
+	//				/ | \
+	//		  B   C  E
+	//	  /	\		|		\
+	//	 D   F	G   |
+	//	 		 	\		 /
+	//				  --
 	graph.AddEdge(&nA, &nB)
-	graph.AddEdge(&nA, &nC)
-	graph.AddEdge(&nA, &nE)
-	graph.AddEdge(&nB, &nD)
+	graph.AddEdge(&nA, &nG)
+	graph.AddEdge(&nA, &nH)
+	graph.AddEdge(&nB, &nC)
 	graph.AddEdge(&nB, &nF)
-	graph.AddEdge(&nC, &nG)
-	graph.AddEdge(&nE, &nF)
+	graph.AddEdge(&nC, &nD)
+	graph.AddEdge(&nC, &nE)
+	graph.AddEdge(&nH, &nI)
+	graph.AddEdge(&nH, &nL)
+	graph.AddEdge(&nI, &nJ)
+	graph.AddEdge(&nI, &nK)
+	//						A
+	//					/ | \
+	//				B		G		H
+	//			/	\			/		\
+	//		C		F		I			L
+	//	/	\			/	\
+	// D	E		J		K
 	return &graph
 }
 
 func main() {
 	graph := fillGraph()
-	graph.BFS()
-	fmt.Println("----")
+	// graph.BFS()
+	// fmt.Println("----")
 	graph.DFS()
 }
