fea448a85050fd4494e58aecac79fd166c901bc8
Valentin Omnès <valentin.omnes@gmail.com>
2020-02-07 16:40:03 +0100

    doc: Comment heap sort algo

--- src/algorithms/sort/heap_sort.go
+++ src/algorithms/sort/heap_sort.go
@@ -10,14 +10,18 @@ func HeapSort(arr []int) []int {
 		return arr
 	}
 	len := len(arr)
+	// Create the heap
 	heap := models.Heap{}
 	h := heap.New(len, models.HeapMin)
+	// Add nodes with the arr values in the heap
 	for _, e := range arr {
 		h.Insert(e)
 	}
+	// Allocat sorted array
 	sortedArray := make([]int, h.GetCapacity())
 	index := 0
 	for {
+		// Store the root value then remove it
 		sortedArray[index] = h.Delete(0)
 		if h.IsEmpty() {
 			break
